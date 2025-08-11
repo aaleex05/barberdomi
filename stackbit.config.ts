04bc131f319372db2bc3a99d971b0edbfb4bd918
Alex Aperador Salcedo <alexapsa2007@gmail.com>
2025-08-11 21:06:55 +0200

    cambios

---
+++ stackbit.config.ts
@@ -0,0 +1,24 @@
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "^0.7.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [
+            { name: "title", type: "string", required: true }
+          ]
+        }
+      ],
+    })
+  ]
+});
