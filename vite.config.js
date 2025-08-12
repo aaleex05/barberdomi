436f3279f64df039238db4f5e7040b6f49b6ed67
Alex Aperador Salcedo <alexapsa2007@gmail.com>
2025-08-12 19:30:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 // vite.config.js
 export default {
   server: {
-    host: true, // Esto permite que el servidor sea accesible desde la red.
-    port: 5173, // Asegura que el puerto sea el 5173, el predeterminado de Vite.
+    host: true, 
     allowedHosts: [
       'devserver-preview--thebarberdomi.netlify.app'
     ]
