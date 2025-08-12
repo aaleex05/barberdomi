ac7c3d71a7d64cf8b1411e7348365cad9ae9c421
Alex Aperador Salcedo <alexapsa2007@gmail.com>
2025-08-12 19:31:03 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-    server: {
-      allowedHosts: process.env.ALLOWED_HOSTS
-        ? process.env.ALLOWED_HOSTS.split(',')
-        : []
-    }
-  }
+    vite: {
+        plugins: [tailwindcss()],
+      },
 });
